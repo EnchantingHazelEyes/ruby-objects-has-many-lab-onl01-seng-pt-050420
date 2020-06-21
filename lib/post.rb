576918a896fad3c4973ebe66ef3e49a1e2c0dca1
Samantha Barros <samantha.barros95@gmail.com>
2020-06-21 04:47:06 +0000

    Automatically backed up by Learn

--- lib/post.rb
+++ lib/post.rb
@@ -5,8 +5,17 @@ attr_accessor :title, :author
 
   def initialize(title)
     @title = title
+    save
   end 
   
+   def save
+    @@all << self
+  end
+ 
+  def self.all
+    @@all
+  end
+
 
 
 end 
