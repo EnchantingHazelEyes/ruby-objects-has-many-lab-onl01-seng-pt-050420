57a02420bd9a4c3689d0641c01b62118f54bf7ce
Samantha Barros <samantha.barros95@gmail.com>
2020-06-21 05:02:48 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -15,8 +15,8 @@ attr_accessor :name
   end
   
   def add_song_by_name
-     def add_song_by_name(name, genre)
-    song = Song.new(name, genre)
+     def add_song_by_name(name)
+    song = Song.new(name)
     add_song(song)
   end
 
