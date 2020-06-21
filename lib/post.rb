401791e042fe5c23e52960898bc70fa215cc70f3
Samantha Barros <samantha.barros95@gmail.com>
2020-06-21 05:23:36 +0000

    Automatically backed up by Learn

--- lib/post.rb
+++ lib/post.rb
@@ -17,7 +17,7 @@ attr_accessor :title, :author
   end
   
   def author_name
-    if author.name.include?(author)
+    if author.name.include?(@author)
       self.author.name
     else 
       author = nil
