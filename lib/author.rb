80c050dbb36349a658b9585c48a8e5606e599c65
Samantha Barros <samantha.barros95@gmail.com>
2020-06-21 05:43:36 +0000

    Automatically backed up by Learn

--- lib/author.rb
+++ lib/author.rb
@@ -1,6 +1,7 @@
 class Author 
 
 attr_accessor :name 
+@@post_count = 0
 
   def initialize(name)
     @name = name 
@@ -8,6 +9,7 @@ attr_accessor :name
 
  def add_post(post)
     post.author = self
+    @@post_count +=1
   end
  
   def posts
@@ -17,7 +19,11 @@ attr_accessor :name
   def add_post_by_title(name)
     post = Post.new(name)
     add_post(post)
+    @@post_count +=1
   end  
 
+   def self.post_count
+    @@post_count
+   end 
 
 end 
