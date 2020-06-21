6140ca6bebfd947bb4301463cdd1a98bea3fa996
Samantha Barros <samantha.barros95@gmail.com>
2020-06-21 04:43:13 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -6,6 +6,13 @@ attr_accessor :name
     @name = name 
   end 
   
+  def add_song(song)
+    song.artist = self
+  end
+ 
+  def songs
+    Song.all.select {|song| song.artist == self}
+  end
 
 
 end 
